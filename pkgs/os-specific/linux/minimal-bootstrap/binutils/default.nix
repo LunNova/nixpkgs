155b0fc27f78931dc04d3d336882eec45e9c9168
dish <pyrox@pyrox.dev>
2025-09-05 19:50:51 -0400

    minimal-bootstrap.binutils: 2.41 -> 2.45
    2.42: https://lists.gnu.org/archive/html/info-gnu/2024-01/msg00016.html
    2.43: https://lists.gnu.org/archive/html/info-gnu/2024-08/msg00001.html
    2.43.1:
    https://lists.gnu.org/archive/html/info-gnu/2024-08/msg00003.html
    2.44: https://lists.gnu.org/archive/html/info-gnu/2025-02/msg00001.html
    2.45: https://lists.gnu.org/archive/html/info-gnu/2025-07/msg00009.html
    
    Note that releases 2.44+ do not include sources for the GOLD linker,
    because it is deprecated, but that doesn't matter for us, since we never
    used it.

--- pkgs/os-specific/linux/minimal-bootstrap/binutils/default.nix
+++ pkgs/os-specific/linux/minimal-bootstrap/binutils/default.nix
@@ -19,11 +19,11 @@
 let
   # Based on https://github.com/ZilchOS/bootstrap-from-tcc/blob/2e0c68c36b3437386f786d619bc9a16177f2e149/using-nix/2a1-static-binutils.nix
   pname = "binutils";
-  version = "2.41";
+  version = "2.45";
 
   src = fetchurl {
     url = "mirror://gnu/binutils/binutils-${version}.tar.xz";
-    hash = "sha256-rppXieI0WeWWBuZxRyPy0//DHAMXQZHvDQFb3wYAdFA=";
+    hash = "sha256-xQwOf5yxiJgOLMl+RTdiaxZyRBgVWH8eq2nSob++9dI=";
   };
 
   patches = [
